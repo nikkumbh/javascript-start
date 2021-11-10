e00466a4a00db2b396ff6d839768e7d11eef6682
prabal nikkumbh <prabalnikkumbh123@gmail.com>
2021-11-10 12:43:02 +0530

    added lexical scope file

---
+++ fn.js
@@ -0,0 +1,42 @@
+// console.log("line no 1", varName);
+//here variable varname is also a lexical scope as its globally declared
+
+// var varName = 10;
+
+// function b(){
+//     console.log("line no 5", varName);
+// }
+// console.log("line no 7" , varName);
+// function fn(){
+//     console.log("line no 9", varName);
+//     var varName = 20;
+//     b();
+//     console.log("line no 13", varName)
+// }
+// fn();
+
+
+// for teaching about lexical scope.
+// let variable = 2;
+// function parent(){
+//     function child(){
+//         console.log(variable);
+//     }
+//     child();
+// }
+// parent();
+
+
+console.log("line no 1", varName);
+var varName = 10;
+console.log("line no 3", varName);
+function fn(){
+    console.log("line no 5", varName);
+    var varName = 20;
+    function b(){
+        console.log("line no 8", varName);
+    }
+    b();
+    console.log(varName);
+}
+fn();
